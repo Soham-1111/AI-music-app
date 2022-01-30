6fe3b75eb646dc37c7a6b972edf556886746822b
Soham-1111 <73647733+Soham-1111@users.noreply.github.com>
2022-01-30 19:47:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 function setup(){
     canvas= createCanvas(500, 400);
     canvas.position(500, 300);
+    video= createCapture(VIDEO); 
+    video.hide(); 
+
+
+}
+music1=""; 
+music2="";
+
+function draw(){
+    image(video, 0,0, 500, 400); 
+}
+
+function preload(){
+   music1.loadSound(music.mp3);
+   music2= loadSound(Rarin_YESSIR_.mp3); 
 }
